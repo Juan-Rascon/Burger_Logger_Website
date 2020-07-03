b0f9c071ec0028f2bf25224af044a0edbfc0e7ee
jcr <jcrascon+github@gmail.com>
2020-07-02 18:15:34 -0700

    initial set up of environment

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Santa Barbara Char', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tommys', false);
